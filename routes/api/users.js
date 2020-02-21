5d4227f594920c386e01f8ae212a944759a5f7af
TheJamylle <jamyllemagal@gmail.com>
2020-02-21 15:05:20 -0300

    users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//GET api/users
-router.get('/', (req, res) => res.send('User route'));
+//POST api/users
+router.post('/',[
+    check('nome', 'Nome é requerido').not().isEmpty(),
+    check('email', 'Email é requerido').isEmail(),
+    check('password', 'Coloque uma senha de no mínimo 6 caracteres').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+});
 
 module.exports = router;
