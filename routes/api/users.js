712b687bf8bcc426746b0d07cd7b2c5bfeacf09e
TheJamylle <jamyllemagal@gmail.com>
2020-02-21 17:36:16 -0300

    jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -45,7 +47,21 @@ router.post('/',[
 
         await user.save();
 
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload, 
+            config.get('jwtToken'), 
+            { expiresIn: 3600 }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token })
+            }
+        );
+
     } catch(err){
         console.error(err.message);
         res.status(500).send('Server error');
